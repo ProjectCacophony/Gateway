5d1b8604a55e5999c96714c59e642a1c8a159ac4
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-02-07 21:02:55 +0100

    wiping the repo 👋

--- go_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-PKGS=()
-while read -r pkg; do
-  PKGS+=( "${pkg}" )
-done < <(go list ./... | grep -v '/vendor/')
-
-echo "Will test ${#PKGS} packages."
-
-go test -v -race "${PKGS[@]}"
