970afa574c80cb5a35c919ab9ffd4a4bcb3df080
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-02-15 22:40:32 +0100

    updates Deployment

--- k8s/render.sh
+++ k8s/render.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # should have the following environment variables set:
-# DISCORD_TOKEN
+# DISCORD_TOKENS
 # AMQP_DSN
 # LOGGING_DISCORD_WEBHOOK
 # PORT
@@ -12,7 +12,7 @@ template="k8s/manifest.tmpl.yaml"
 target="k8s/manifest.yaml"
 
 cp "$template" "$target"
-sed -i -e "s|{{DISCORD_TOKEN}}|$DISCORD_TOKEN|g" "$target"
+sed -i -e "s|{{DISCORD_TOKENS}}|$DISCORD_TOKENS|g" "$target"
 sed -i -e "s|{{AMQP_DSN}}|$AMQP_DSN|g" "$target"
 sed -i -e "s|{{LOGGING_DISCORD_WEBHOOK}}|$LOGGING_DISCORD_WEBHOOK|g" "$target"
 sed -i -e "s|{{PORT}}|$PORT|g" "$target"
